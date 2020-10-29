2cc5ffcf40cd4b3e41a6463592e3ec6fe5a2c7cd
Rafael <Bytheway0708@gmail.com>
2020-10-29 12:48:25 -0400

    Test Suit
    Brief explanation of what happened here

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { Container } from "react-bootstrap";
+import App from "./App";
+const Div= (props:any) =>(<div></div>)
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Div/>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
